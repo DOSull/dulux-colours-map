25e0cfb79d1ceb34e6120b786e59183754739fd7
DOSull <david.osullivan@vuw.ac.nz>
2021-09-11 10:20:04 +1200

    included triangulation, idw and spline interpolated maps

--- README.md
+++ README.md
@@ -1,11 +1,16 @@
 # A web map from the Dulux colours of New Zealand
-This is just a demonstration of the flexibility of the R ecosystem for quickly putting together a [web map](https://dosull.github.io/dulux-colours-map/map/).
+This is just a demonstration of the flexibility of the R ecosystem for quickly putting together a [web map](https://dosull.github.io/dulux-colours-map/maps/).
 
 The [map](https://dosull.github.io/dulux-colours-map/map/) is a somewhat silly take on this [range of paints](https://www.dulux.co.nz/colour/colours-of-new-zealand).
 
-The R code used to make the map can be viewed [here](code/build-dulux-colours-map.md).
+Some alternative maps where I've interpolated RGB values at point locations to make a smoothed raster colour map are also available:
 
-The annotated output from the [RMarkdown](https://rmarkdown.rstudio.com/) of [the code](build-dulux-colours-map.Rmd) can be [viewed here](https://dosull.github.io/dulux-colours-map/code/).
++ [Triangulation](https://dosull.github.io/dulux-colours-map/maps/triangulation.html) based using `akima::interp` with `linear = TRUE`
++ [Inverse-distance weighting](https://dosull.github.io/dulux-colours-map/maps/triangulation.html) using `spatstat::idw` with `power = 4`
++ [Thin plate splines](ttps://dosull.github.io/dulux-colours-map/maps/splines.html) using `fields::Tps` with `m = 3`
 
-## Maptime slides
-I made [some slides](https://dosull.github.io/dulux-colours-map/slides/) from the code again using RMarkdown&mdash;a nice thing to learn more about!
+The R code used to make the maps can be viewed [here](code/build-dulux-colours-map.md).
+
+Running the code will produce quite a few data files, which are _not_ posted here.
+
+The annotated output of the [RMarkdown](https://rmarkdown.rstudio.com/) of the code can be [viewed here](https://dosull.github.io/dulux-colours-map/code/). This was lightly modified to make [these slides](https://dosull.github.io/dulux-colours-map/slides/) for a Maptime! presentation in September 2021.
--- interpolate-colours.R
+++ interpolate-colours.R
@@ -3,39 +3,34 @@ library(raster)
 library(dplyr)
 library(tidyr)
 library(stringr)
-library(data.table)
+library(fields)
+library(tmap)
+tmap_mode("plot")
+library(spatstat)
+library(maptools)
+library(akima)
 
 setwd("~/Documents/code/dulux-colours-map")
 
-get_CMYK <- Vectorize(function(R, G, B) {
-  Rn <- R / 255
-  Gn <- G / 255
-  Bn <- B / 255
-  K <- 1 - max(Rn, Gn, Bn)
-  C <- ifelse(K == 1, 0, (1 - Rn - K) / (1 - K))
-  M <- ifelse(K == 1, 0, (1 - Gn - K) / (1 - K))
-  Y <- ifelse(K == 1, 0, (1 - Bn - K) / (1 - K))
-  return(list(C = C, M = M, Y = Y, K = K));
-})
 
-get_RGB <- Vectorize(function(C, M, Y, K, component = "red") {
-  if (component == "red") {
-    return(255 * (1 - C) * (1 - K))
-  }
-  if (component == "green") {
-    return(255 * (1 - M) * (1 - K))
-  }
-  return(255 * (1 - Y) * (1 - K))
-})
+get_rescale <- function(r1, r2) {
+  range1 <- r1[2] - r1[1]
+  range2 <- r2[2] - r2[1]
+  scale <- range2 / range1
+  return (function(x) {
+    return(r2[1] + scale * (x - r1[1]))
+  })
+}
 
 
 nz <- st_read("nz.gpkg")
 
 r <- nz %>% 
-  st_make_grid(cellsize = 5000, what = "centers") %>%
+  st_make_grid(cellsize = 2500, what = "centers") %>%
   st_coordinates() %>%
   as_tibble() %>%
-  mutate(Z = 0) %>%
+  rename(x = X, y = Y) %>%
+  mutate(z = 0) %>%
   rasterFromXYZ()
 
 dulux_pts <- st_read("dulux-colours-pts.gpkg")
@@ -43,35 +38,26 @@ dulux_pts <- st_read("dulux-colours-pts.gpkg")
 rgb <- dulux_pts %>%
   st_coordinates() %>%
   as_tibble() %>%
-  mutate(red = dulux_pts$red, green = dulux_pts$green, blue = dulux_pts$blue) %>%
-  mutate(cyan = 0, magenta = 0, yellow = 0, black = 0)
+  # rename(x = X, y = Y) %>%
+  mutate(red = dulux_pts$red, green = dulux_pts$green, blue = dulux_pts$blue)
 
-## NO IDEA WHY THIS IS SO ARCANE!
-rgb[, c("cyan", "magenta", "yellow", "black")] <- as_tibble(t(get_CMYK(rgb$red, rgb$green, rgb$blue)))
-rgb <- rgb %>%
-  mutate(across(where(is.list), as.numeric))
-
-library(fields)
-
-components <- c("cyan", "magenta", "yellow", "black")
+components <- c("red", "green", "blue")
 layers <- list()
 for (component in components) {
-  spline <- Tps(rgb[, 1:2], rgb[[component]], scale.type = "unscaled")
+  spline <- Tps(rgb[, 1:2], rgb[[component]], scale.type = "unscaled", m = 3)
   layers[[component]] <- interpolate(r, spline)
 }
-spline_cmyk <- brick(layers)
+rgb.s <- brick(layers)
+crs(rgb.s) <- st_crs(nz)$wkt
+rgb.s <- mask(rgb.s, nz)
 
+rgb.s$red <- get_rescale(c(minValue(rgb.s$red), maxValue(rgb.s)), 
+                         range(rgb$red))(rgb.s$red)
+rgb.s$green <- get_rescale(c(minValue(rgb.s$green), maxValue(rgb.s$green)), 
+                           range(rgb$green))(rgb.s$green)
+rgb.s$blue <- get_rescale(c(minValue(rgb.s$blue), maxValue(rgb.s$blue)), 
+                          range(rgb$blue))(rgb.s$blue)
 
-library(tmap)
-tmap_mode("view")
-
-tm_shape(spline_rgb) + 
-  tm_rgb() +
-  tm_shape(nz) + 
-  tm_borders(col = "blue", lwd = 0.1)
-  
-library(spatstat)
-library(maptools)
 
 W <- nz$geom %>%
   st_buffer(1000) %>%
@@ -84,12 +70,59 @@ for (component in components) {
   pp <- ppp(x = rgb$X, y = rgb$Y, window = W, marks = rgb[[component]])
   layers[[component]] <- raster(idw(pp, eps = 2500, power = 4))
 }
-idw.rgb <- brick(layers)
-crs(idw.rgb) <- st_crs(nz)$wkt
+rgb.idw <- brick(layers)
+crs(rgb.idw) <- st_crs(nz)$wkt
 
-tm_shape(idw.rgb) +
+vor <- st_read("dulux-colours-vor.gpkg")
+
+m1 <- tm_shape(vor) + 
+  tm_polygons(col = "rgb", border.col = "grey", lwd = 0.1) +
+  tm_layout(title = "Voronoi")
+m2 <- tm_shape(rgb.idw) +
+  tm_rgb() +
+  tm_layout(title = "IDW of rgb")
+m3 <- tm_shape(rgb.s) +
   tm_rgb() + 
-  tm_shape(nz) + 
-  tm_borders(col = "blue", lwd = 0.1)
-  # tm_shape(dulux_pts) +
-  # tm_dots(col = "rgb")
+  tm_layout(title = "Spline of rgb")
+
+tmap_arrange(m1, m2, m3, nrow = 1)
+
+
+tri.r <- interp(rgb$X, rgb$Y, rgb$red,
+                nx = 402, ny = 591, linear = TRUE)
+tri.g <- interp(rgb$X, rgb$Y, rgb$green,
+                nx = 402, ny = 591, linear = TRUE)
+tri.b <- interp(rgb$X, rgb$Y, rgb$blue,
+                nx = 402, ny = 591, linear = TRUE)
+
+rgb.t <- brick(list(red = raster(tri.r), 
+                    green = raster(tri.g), 
+                    blue = raster(tri.b)))
+crs(rgb.t) <- st_crs(nz)$wkt
+rgb.t <- mask(rgb.t, nz)
+
+tm_shape(rgb.t) + 
+  tm_rgb()
+
+
+get_CMYK <- Vectorize(function(R, G, B) {
+  Rn <- R / 255
+  Gn <- G / 255
+  Bn <- B / 255
+  K <- 1 - max(Rn, Gn, Bn)
+  C <- ifelse(K == 1, 0, (1 - Rn - K) / (1 - K))
+  M <- ifelse(K == 1, 0, (1 - Gn - K) / (1 - K))
+  Y <- ifelse(K == 1, 0, (1 - Bn - K) / (1 - K))
+  return(list(C = C, M = M, Y = Y, K = K));
+})
+
+get_R <- Vectorize(function(C, M, Y, K) {
+  return(255 * (1 - C) * (1 - K))
+})
+get_G <- Vectorize(function(C, M, Y, K) {
+  return(255 * (1 - M) * (1 - K))
+})
+get_B <- Vectorize(function(C, M, Y, K) {
+  return(255 * (1 - Y) * (1 - K))
+})
+
